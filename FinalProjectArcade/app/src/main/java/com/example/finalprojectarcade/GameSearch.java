042990727a07ededc30a474c2d2389eb092cfd08
infinite2004 <44120607+infinite2004@users.noreply.github.com>
2020-06-13 15:05:02 -0400

    i have updated the xml

--- FinalProjectArcade/app/src/main/java/com/example/finalprojectarcade/GameSearch.java
+++ FinalProjectArcade/app/src/main/java/com/example/finalprojectarcade/GameSearch.java
@@ -1,33 +1,18 @@
 package com.example.finalprojectarcade;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameSearch extends AppCompatActivity {
  EditText searchBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_search);
 
-        searchBar = findViewById(R.id.search_bar);
     }
 
-    public void fetchData(View view) {
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        String GameSearched = searchBar.getText().toString();
-
-
-    }
 }
